ee67174f41df8d475bf4478ee923bc5111648522
volkanbasaran1 <volkanba12@gmail.com>
2024-01-31 17:36:03 +0300

    first commit

---
+++ src/constants/constants.js
@@ -0,0 +1,11 @@
+export const options = {
+  headers: {
+    accept: "application/json",
+    Authorization:
+      //   kimlik doğrulama header'ı
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMGY5NDY2MWZkYjhlM2IyNTliYmFkNzE1ZDMyOTQxNyIsInN1YiI6IjY1YjhjNDRjOTBmY2EzMDE2MjA2MTYxNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KhIsavgSt2aUo37hU8ZBlvszRxKne2FOlbdZB8q1yuw",
+  },
+};
+
+// resimler için baseURL
+export const baseImageURL = "https://image.tmdb.org/t/p/original";
